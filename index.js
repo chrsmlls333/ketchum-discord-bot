95ff32604065b336662e90dd25d2c8f4d85acceb
Chris Mills <chris@chriseugenemills.com>
2020-03-20 20:39:42 -0700

    Remove dotenv and add config.json

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require('dotenv').config()
+/*
 
+*/
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
+});
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(token);
