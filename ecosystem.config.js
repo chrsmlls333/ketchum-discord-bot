2e74cae9c4d1462cb12b7caa625d47a17a3973de
Chris Mills <chris@chriseugenemills.com>
2020-03-21 20:04:19 -0700

    Define PM2 to fix log watching infinite restart

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name: 'discord-downloader',
+    script: 'index.js',
+
+    // args: "",
+
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    watch_delay: 1000,
+    ignore_watch: ["node_modules", "logs", "debug_message.json"],
+
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
+
+// Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
