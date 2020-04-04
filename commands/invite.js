4418cde8c365c0e4e24304c959100655b0be3647
chrsmlls333 <chrsmlls333@gmail.com>
2020-04-04 18:57:11 -0500

    Add Invite command

---
+++ commands/invite.js
@@ -0,0 +1,29 @@
+// const Discord = require('discord.js');
+// const logger = require('winston');
+
+const utils = require('../utils');
+
+
+module.exports = {
+
+  name: 'invite',
+  aliases: ['getinvite'],
+
+  description: 'Get a link for inviting this bot to another server you have Manage Server permissions on...',
+
+  guildOnly: false,
+
+  args: false,
+  
+  execute(message) {
+
+    utils.generateInvite(message.client)
+      .then(link => {
+        const embed = utils.embedTemplate(message.client)
+          .setTitle(`Invite ${message.client.user.username} to other servers!`)
+          .setURL(link);
+        return message.channel.send(embed);
+      });
+    
+  },
+};
